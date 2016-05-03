187720da17fcb808172808e8978b520d14b9016c
Joe Nilson <joenilson@gmail.com>
2016-05-03 18:14:15 -0400

    Fix menor v4 al buscador de NCF, falta completar el boton de agregar a cliente si no esta en el listado y si existe también mostrarlo

--- controller/buscador_rnc.php
+++ controller/buscador_rnc.php
@@ -31,6 +31,8 @@ class buscador_rnc extends fs_controller{
     public $detalle;
     public $rnc;
     public $nombre;
+    public $viewstate;
+    public $eventvalidation;
 
     public function __construct($name = '', $title = 'home', $folder = '', $admin = FALSE, $shmenu = TRUE, $important = FALSE) {
         parent::__construct(__CLASS__, 'Buscador de RNC', 'contabilidad', FALSE, FALSE, TRUE);
@@ -39,6 +41,7 @@ class buscador_rnc extends fs_controller{
     protected function private_core() {
         $this->resultados = false;
         $this->total_resultados = 0;
+        
         $tipo = filter_input(INPUT_POST, 'tipo');
         if(!empty($tipo)){
             switch ($tipo){
@@ -52,11 +55,48 @@ class buscador_rnc extends fs_controller{
                     break;
             }
         }
-
-
+    }
+    
+    //Pedimos que nos den el VIEWSTATE y el EVENTVALIDATION a la página de busqueda
+    public function autorizacion(){
+        $h = curl_init();
+        curl_setopt($h, CURLOPT_URL, 'http://www.dgii.gov.do/app/WebApps/Consultas/rnc/RncWeb.aspx');
+        curl_setopt($h, CURLOPT_HEADER, false);
+        curl_setopt($h, CURLOPT_RETURNTRANSFER, 1);
+        $result = curl_exec($h);
+        curl_close($h);
+        $html = str_get_html($result);
+        $this->viewstate = $html->getElementById('#__VIEWSTATE',0)->value;
+        $this->eventvalidation = $html->getElementById('#__EVENTVALIDATION',0)->value;
+    }
+    
+    //Si la busqueda no es por RNC y en su lugar es por nombre actualizamos viewstate y eventvalidation
+    public function actualizacion_autorizacion($tipoBusqueda){
+        $post = array(
+            '__EVENTTARGET'=>'rbtnlTipoBusqueda$1',
+            '__EVENTARGUMENT'=>"",
+            '__LASTFOCUS'=>"",
+            '__VIEWSTATE'=>$this->viewstate,
+            '__EVENTVALIDATION'=>$this->eventvalidation,
+            'rbtnlTipoBusqueda' => $tipoBusqueda,
+            'txtRncCed'=>''
+        );
+        $query = http_build_query($post);
+        $h = curl_init();
+        curl_setopt($h, CURLOPT_URL, 'http://www.dgii.gov.do/app/WebApps/Consultas/rnc/RncWeb.aspx');
+        curl_setopt($h, CURLOPT_POST, true);
+        curl_setopt($h, CURLOPT_POSTFIELDS, $query);
+        curl_setopt($h, CURLOPT_HEADER, false);
+        curl_setopt($h, CURLOPT_RETURNTRANSFER, 1);
+        $result = curl_exec($h);
+        curl_close($h);
+        $html = str_get_html($result);
+        $this->viewstate = $html->getElementById('#__VIEWSTATE',0)->value;
+        $this->eventvalidation = $html->getElementById('#__EVENTVALIDATION',0)->value;
     }
 
     public function buscar(){
+        $this->autorizacion();
         $rnc = filter_input(INPUT_POST, 'rnc');
         $nombre = filter_input(INPUT_POST, 'nombre');
         $tipoBusqueda = (!empty($rnc))?0:1;
@@ -65,12 +105,15 @@ class buscador_rnc extends fs_controller{
         $this->nombre = $nombre;
         $campo = (!empty($rnc))?'txtRncCed':'txtRazonSocial';
         $boton = (!empty($rnc))?'btnBuscaRncCed':'btnBuscaRazonSocial';
+        if($tipoBusqueda == 1){
+            $this->actualizacion_autorizacion($tipoBusqueda);
+        }
         $post = array(
             '__EVENTTARGET'=>"",
             '__EVENTARGUMENT'=>"",
-            '__LASTFOCUS'>"",
-            '__VIEWSTATE'=>($tipoBusqueda==0)?"/wEPDwUKMTY4ODczNzk2OA9kFgICAQ9kFgQCAQ8QZGQWAWZkAg0PDxYCHgdWaXNpYmxlZ2QWBAIBDw8WAh4EVGV4dGVkZAIDDzwrAAsBAA8WCh4IRGF0YUtleXMWAB4LXyFJdGVtQ291bnQCAR4JUGFnZUNvdW50AgEeFV8hRGF0YVNvdXJjZUl0ZW1Db3VudAIBHwBnZBYCZg9kFgICAQ9kFgxmDw8WAh8BBQkxMzAwMTI0MDdkZAIBDw8WAh8BBSRJTkRVU1RSSUFTIFNBTiBNSUdVRUwgREVMIENBUklCRSBTIEFkZAICDw8WAh8BBSBJTkRVU1RSSUFTIFNBTiBNSUdVRUwgREVMIENBUklCRWRkAgMPDxYCHwEFD0RFUy4gRlJPTlRFUkEgIGRkAgQPDxYCHwEFBk5PUk1BTGRkAgUPDxYCHwEFBkFDVElWT2RkZCvF5/rOh+O96q82YrDFN/M4w6Bp":"/wEPDwUKMTY4ODczNzk2OA9kFgICAQ9kFggCAQ8QZGQWAQIBZAIFDw8WAh4HVmlzaWJsZWhkFgICAw8PFgIeBFRleHQFCTEzMDAxMjQwN2RkAgcPDxYCHwBnZGQCDQ8PFgIfAGdkFgQCAQ8PFgIfAWVkZAIDDzwrAAsBAA8WCh4IRGF0YUtleXMWAB4LXyFJdGVtQ291bnQCCh4JUGFnZUNvdW50AgEeFV8hRGF0YVNvdXJjZUl0ZW1Db3VudAIKHwBnZBYCZg9kFhQCAQ9kFgxmDw8WAh8BBQkxMDE1MjA1NzRkZAIBDw8WAh8BBRRTQU4gTUlHVUVMICYgQ0lBIFNSTGRkAgIPDxYCHwEFBiZuYnNwO2RkAgMPDxYCHwEFAiAgZGQCBA8PFgIfAQUGTk9STUFMZGQCBQ8PFgIfAQUGQUNUSVZPZGQCAg9kFgxmDw8WAh8BBQswMDExODU0MzAyNGRkAgEPDxYCHwEFGlNBTiBNSUdVRUwgQUxDQU5UQVJBIE1BVE9TZGQCAg8PFgIfAQUGJm5ic3A7ZGQCAw8PFgIfAQUCICBkZAIEDw8WAh8BBQZOT1JNQUxkZAIFDw8WAh8BBQZBQ1RJVk9kZAIDD2QWDGYPDxYCHwEFCTEzMTI0MTQwOWRkAgEPDxYCHwEFJFNBTiBNSUdVRUwgQVJDQU5HRUwgQVVUTyBJTVBPUlQgRUlSTGRkAgIPDxYCHwEFH1NBTiBNSUdVRUwgQVJDQU5HRUwgQVVUTyBJTVBPUlRkZAIDDw8WAh8BBQIgIGRkAgQPDxYCHwEFBk5PUk1BTGRkAgUPDxYCHwEFBkFDVElWT2RkAgQPZBYMZg8PFgIfAQULMDAyMDE0Mzg3MzZkZAIBDw8WAh8BBSBTQU4gTUlHVUVMIEFSQ0FOR0VMIFJPQSBNQVJUSU5FWmRkAgIPDxYCHwEFBiZuYnNwO2RkAgMPDxYCHwEFAiAgZGQCBA8PFgIfAQUGTk9STUFMZGQCBQ8PFgIfAQUGQUNUSVZPZGQCBQ9kFgxmDw8WAh8BBQk1MDEzNDA2MTlkZAIBDw8WAh8BBRdTQU4gTUlHVUVMIENBTUlOTyBNQVJJQWRkAgIPDxYCHwEFBiZuYnNwO2RkAgMPDxYCHwEFAiAgZGQCBA8PFgIfAQUDTi9EZGQCBQ8PFgIfAQUISU5BQ1RJVk9kZAIGD2QWDGYPDxYCHwEFCTUzMDAxNjY4N2RkAgEPDxYCHwEFElNBTiBNSUdVRUwgRVVHRU5JT2RkAgIPDxYCHwEFBiZuYnNwO2RkAgMPDxYCHwEFAiAgZGQCBA8PFgIfAQUGTk9STUFMZGQCBQ8PFgIfAQUGQUNUSVZPZGQCBw9kFgxmDw8WAh8BBQkxMzA1MDYxMjRkZAIBDw8WAh8BBRlTQU4gTUlHVUVMIElORFVTVFJJQUwgUyBBZGQCAg8PFgIfAQUGJm5ic3A7ZGQCAw8PFgIfAQUCICBkZAIEDw8WAh8BBQZOT1JNQUxkZAIFDw8WAh8BBQZBQ1RJVk9kZAIID2QWDGYPDxYCHwEFCTUwMTY3NjIxOGRkAgEPDxYCHwEFFFNBTiBNSUdVRUwgTUFSQ0VMSU5PZGQCAg8PFgIfAQUGJm5ic3A7ZGQCAw8PFgIfAQUCICBkZAIEDw8WAh8BBQNOL0RkZAIFDw8WAh8BBQhJTkFDVElWT2RkAgkPZBYMZg8PFgIfAQUJNTAxMTM1NzQ4ZGQCAQ8PFgIfAQUYU0FOIE1JR1VFTCBQQU5ETyBDQU5ESURPZGQCAg8PFgIfAQUGJm5ic3A7ZGQCAw8PFgIfAQUCICBkZAIEDw8WAh8BBQNOL0RkZAIFDw8WAh8BBQhJTkFDVElWT2RkAgoPZBYMZg8PFgIfAQUJMTAxNzQ1NjU3ZGQCAQ8PFgIfAQUVU0FOIE1JR1VFTCBUQUJBQ08gUyBBZGQCAg8PFgIfAQUGJm5ic3A7ZGQCAw8PFgIfAQUCICBkZAIEDw8WAh8BBQNOL0RkZAIFDw8WAh8BBQhJTkFDVElWT2RkZJWjx1jinfz1aeCNQsAF4ThdJVQX",
-            '__EVENTVALIDATION'=>($tipoBusqueda==0)?"/wEWBgK4mJwzApPThYkEAozThYkEAoO8r+cIAqPC2ZIEAsa0/vgKz3FdauzfSiPnd2H2zkLd4PSz+sE=":"/wEWBgLWqfCIAgKT04WJBAKM04WJBAKDvK/nCALrxcShDQKTzMDhBMWBP644j+YvKIDTiad19QWb7ofy",
+            '__LASTFOCUS'=>"",
+            '__VIEWSTATE'=>$this->viewstate,
+            '__EVENTVALIDATION'=>$this->eventvalidation,
             'rbtnlTipoBusqueda' => $tipoBusqueda,
             $campo => $valor_a_buscar,
             $boton => 'Buscar'
